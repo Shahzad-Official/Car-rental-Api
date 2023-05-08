7c97b6f6f0b29a66f12d3e2dc6f7b9c5a35ba036
Muhammad Shahzad <108967038+Shahzad-Official@users.noreply.github.com>
2023-05-08 23:18:18 +0500

    updated

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = express();
 const router = require("./routes/auth");
 const errorHandler = require("./error_handler/error_handler");
 require("dotenv").config();
-const { default: mongoose } = require("mongoose");
+// const { default: mongoose } = require("mongoose");
 const Test = require("./models/user");
 const port=process.env.PORT||3000;
 
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => {
-    console.log("connected");
-  })
-  .catch((err) => {
-    throw Error(err);
-  });
+// mongoose
+//   .connect(process.env.DB_URL)
+//   .then(() => {
+//     console.log("connected");
+//   })
+//   .catch((err) => {
+//     throw Error(err);
+//   });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
